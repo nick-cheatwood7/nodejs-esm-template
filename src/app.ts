f282f1c6e033ba46dc45fbf298818080465d58b3
Nick Cheatwood <nick.cheatwood@gmail.com>
2023-01-09 16:29:14 -0600

    feat: add simple rate limiter

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,13 @@ const app = fastify({
     logger: !__prod__ && { transport: { target: "@fastify/one-line-logger" } }
 });
 
+// Rate limiter
+const ONE_MINUTE = 1000 * 60;
+app.register(import("@fastify/rate-limit"), {
+    max: 100,
+    timeWindow: ONE_MINUTE
+});
+
 // Register routes
 app.register(router, { prefix: "api" });
 
